365d532e8b4621457240421e65a0c4d00051b670
T Mark <tmk1.tran@gmail.com>
2023-10-07 13:01:38 -0500

    updated todo list, further styling of movieList, going to create a branch for features

--- INSTRUCTIONS.md
+++ INSTRUCTIONS.md
@@ -15,14 +15,14 @@ For this weekend challenge you'll be expanding on a movie management application
 
 ## Database Setup
 
-1. Create a database named `saga_movies_weekend`
-2. Run the queries from `database.sql` on the `saga_movies_weekend` database
+[x] 1. Create a database named `saga_movies_weekend`
+[x] 2. Run the queries from `database.sql` on the `saga_movies_weekend` database
 
 ## Install Dependencies
 
-1. `npm install`
-2. `npm run server`
-3. `npm run client`
+[x] 1. `npm install`
+[x] 2. `npm run server`
+[x] 3. `npm run client`
 
 ## Notes
 
@@ -44,13 +44,13 @@ We've added some movie posters in the `public/images` folder, and the database i
 
 This view is completed already! It displays all of the movies in the movie database. 
 
-- TODO: When a movie poster is clicked, a user should be brought to the `/details` view for that movie.
+- [x] TODO: When a movie poster is clicked, a user should be brought to the `/details` view for that movie.
 
 ### Details Page
 
-This should show all details **including ALL genres** for the selected movie, including title, description, and the image, too! Use Sagas and Redux to handle these requests and data.
+[x] This should show all details **including ALL genres** for the selected movie, including title, description, and the image, too! Use Sagas and Redux to handle these requests and data.
 
-- TODO: The details page should have a `Back to List` button, which should bring the user to the Home/List Page
+- [x] TODO: The details page should have a `Back to List` button, which should bring the user to the Home/List Page
 
 > Base functionality does not require the movie details to load correctly after refresh of the browser.
 
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -5,7 +5,7 @@ import { useHistory } from "react-router-dom";
 import "./MovieList.css";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import { Grid, Typography } from "@mui/material";
+import { Grid, Button, Typography } from "@mui/material";
 
 function MovieList() {
   const dispatch = useDispatch();
@@ -17,33 +17,88 @@ function MovieList() {
     dispatch({ type: "FETCH_MOVIES" });
   }, []);
 
-  return (
-    <main>
-      <h1>Movies</h1>
-      <div className="movies">
-        <Grid container spacing={2}>
-          {movies.map((movie) => {
-            return (
-              <Grid item key={movie.id} xs={3}>
-                <div>
-                  <Card style={{ width: "50%" }}>
-                    <CardContent>
-                      <h3>{movie.title}</h3>
-                      <img
-                        style={{ width: "100%", height: "auto" }}
-                        onClick={() => history.push(`/details/${movie.id}`)}
-                        src={movie.poster}
-                        alt={movie.title}
-                      />
-                    </CardContent>
-                  </Card>
-                </div>
-              </Grid>
+  const scrollContainerRef = React.useRef(null);
+  const cardWidth = 220; // Adjust to match card width including margin
+  const numVisibleCards = 3; // Number of cards to display at a time
+  const totalWidth = cardWidth * movies.length;
+
+  const scrollLeft = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.style.transition =
+        "transform 0.5s ease-in-out";
+      scrollContainerRef.current.style.transform = `translateX(${
+        cardWidth * numVisibleCards
+      }px)`; // Scroll 'numVisibleCards' at a time
+
+      setTimeout(() => {
+        if (scrollContainerRef.current) {
+          scrollContainerRef.current.style.transition = "none";
+          scrollContainerRef.current.style.transform = "translateX(0)";
+          for (let i = 0; i < numVisibleCards; i++) {
+            scrollContainerRef.current.appendChild(
+              scrollContainerRef.current.firstElementChild
             );
-          })}
-        </Grid>
+          }
+        }
+      }, 300);
+    }
+  };
+
+  const scrollRight = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.style.transition =
+        "transform 0.5s ease-in-out";
+      scrollContainerRef.current.style.transform = `translateX(-${
+        cardWidth * numVisibleCards
+      }px)`; // Scroll 'numVisibleCards' at a time
+
+      setTimeout(() => {
+        if (scrollContainerRef.current) {
+          scrollContainerRef.current.style.transition = "none";
+          scrollContainerRef.current.style.transform = "translateX(0)";
+          for (let i = 0; i < numVisibleCards; i++) {
+            scrollContainerRef.current.insertBefore(
+              scrollContainerRef.current.lastElementChild,
+              scrollContainerRef.current.firstElementChild
+            );
+          }
+        }
+      }, 300);
+    }
+  };
+
+  return (
+    <div className="movie-row">
+      <h1>Movies</h1>
+      <div
+        className="movie-container"
+        ref={scrollContainerRef}
+        style={{ width: `${totalWidth}px` }}
+      >
+        {movies.map((movie) => (
+          <Grid item key={movie.id} xs={12} sm={6} md={4} lg={3}>
+            <Card className="movie-card">
+              <CardContent>
+                <img
+                  style={{ width: "100%", height: "auto", cursor: "pointer" }}
+                  onClick={() => history.push(`/details/${movie.id}`)}
+                  src={movie.poster}
+                  alt={movie.title}
+                />
+                <br />
+                <br />
+                <Typography variant="h5" style={{fontFamily: "Trajan Pro"}}>{movie.title}</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
       </div>
-    </main>
+      <br />
+      <div>
+        <Button onClick={scrollLeft} variant="contained" style={{marginRight: "5px"}}>Left</Button>
+        <Button onClick={scrollRight} variant="contained">Right</Button>
+      </div>
+    </div>
   );
 }
 

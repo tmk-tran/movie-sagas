9308d2f9664789b3e9b34907f379325fd236bb88
T Mark <tmk1.tran@gmail.com>
2023-10-09 12:22:31 -0500

    added message in search component as it is still under construction

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -4,6 +4,8 @@ export default function Search() {
   return (
     <form>
       <TextField label="Search" variant="filled" fullWidth style={{backgroundColor: "whitesmoke"}} />
+      <h1 style={{color: "orange"}}>Under Construction...</h1>
+      <p style={{color: "orange"}}>Stretch Goal feature coming soon</p>   
     </form>
   );
 }
